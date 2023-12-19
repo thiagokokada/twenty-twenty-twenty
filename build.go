9672776e872108c5c157f964e32420f85b2b79db
Thiago Kenji Okada <thiagokokada@gmail.com>
2023-12-19 12:37:45 +0000

    Simplify build files

--- build.go
+++ build.go
@@ -1,5 +1,5 @@
-//go:build !linux && !darwin
-// +build !linux,!darwin
+//go:build !darwin
+// +build !darwin
 
 //go:generate go build -o bin/twenty-twenty-twenty
 
--- build_linux.go
+++
@@ -1,6 +0,0 @@
-//go:build linux
-// +build linux
-
-//go:generate go build -tags=nowayland,nox11,novulkan -o bin/twenty-twenty-twenty
-
-package main
