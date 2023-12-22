348272fb223a0038c8909315b729439fa6014bd7
Thiago Kenji Okada <thiagokokada@gmail.com>
2023-12-22 20:49:33 +0000

    Add fallback for version

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
 
   outputs = { self, nixpkgs, ... }:
     let
-      version = "nix-${self.shortRev or self.dirtyShortRev}";
+      version = "nix-${self.shortRev or self.dirtyShortRev or "unknown-dirty"}";
 
       supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
 
