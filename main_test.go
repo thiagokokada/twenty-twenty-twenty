87c356847c281292c764c09552d12a79366297c3
Thiago Kenji Okada <thiagokokada@gmail.com>
2024-01-10 18:20:07 +0000

    Add comments about tests

--- main_test.go
+++ main_test.go
@@ -6,13 +6,17 @@ import (
 	"gioui.org/x/notify"
 )
 
+// The reason this test exist is to help with development (e.g.: test if
+// notification is working). It is useless outside of development purposes and
+// needs a proper desktop environment to work, and this is the reason why it is
+// not run in CI.
 func TestSendNotification(t *testing.T) {
 	notifier, err := notify.NewNotifier()
 	if err != nil {
 		t.Fatalf("Error while creating a notifier: %v\n", err)
 	}
 	// ignoring result, because this test does not work in some platforms (e.g.:
-	// darwin)
+	// darwin, because lack of signature)
 	_ = sendNotification(
 		notifier,
 		"Test notification title",
--- notification_sound_test.go
+++ notification_sound_test.go
@@ -5,6 +5,9 @@ import (
 	"time"
 )
 
+// The reason this test exist is to help with development (e.g.: test if sound
+// is working). It is useless outside of development purposes and needs a proper
+// sound environment to work, and this is the reason why it is not run in CI.
 func TestPlayNotificationSound(t *testing.T) {
 	err := initBeep()
 	if err != nil {
