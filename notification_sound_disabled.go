2c1c756949e98565ffebd3a0f6009d8439651917
Thiago Kenji Okada <thiagokokada@gmail.com>
2024-01-15 13:09:12 +0000

    Return something in the channel to make sure we don't block forever

--- notification_sound_disabled.go
+++ notification_sound_disabled.go
@@ -5,6 +5,10 @@ package main
 
 const notificationSoundEnabled bool = false
 
-func playNotificationSound() chan bool { return make(chan bool) }
+func playNotificationSound() chan bool {
+	c := make(chan bool)
+	c <- false
+	return c
+}
 
 func initBeep() error { return nil }
