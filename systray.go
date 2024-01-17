835801db810241c3ac0e7ea3ce1c578d6d0e6158
Thiago Kenji Okada <thiagokokada@gmail.com>
2024-01-17 20:56:27 +0000

    Extract resume twenty-twenty-twenty code in a separate function

--- systray.go
+++ systray.go
@@ -16,6 +16,35 @@ import (
 //go:embed assets/eye_light.png
 var data []byte
 
+func resumeTwentyTwentyTwentyAfter(
+	ctx context.Context,
+	ctxCancel context.CancelFunc,
+	after time.Duration,
+	mEnabled *systray.MenuItem,
+	mPause *systray.MenuItem,
+) {
+	log.Printf("Pausing twenty-twenty-twenty for %.f hour...\n", after.Hours())
+	mainCtxCancel() // cancelling current twenty-twenty-twenty goroutine
+	timer := time.NewTimer(after)
+
+	select {
+	case <-timer.C:
+		notification := sendNotification(
+			notifier,
+			"Resuming 20-20-20",
+			fmt.Sprintf("You will see a notification every %.f minutes(s)", frequency.Minutes()),
+			notificationSound,
+		)
+		cancelNotificationAfter(notification, duration, notificationSound)
+		runTwentyTwentyTwenty(notifier, duration, frequency, notificationSound)
+
+		mEnabled.Enable()
+		mPause.Uncheck()
+	case <-ctx.Done():
+	}
+	ctxCancel() // make sure the current context is closed
+}
+
 func onReady() {
 	systray.SetIcon(data)
 	systray.SetTooltip("TwentyTwentyTwenty")
@@ -27,54 +56,42 @@ func onReady() {
 	}
 	systray.AddSeparator()
 	mQuit := systray.AddMenuItem("Quit", "Quit the whole app")
-	ctx, ctxCancel := context.WithCancel(context.Background())
+	var ctx context.Context
+	var ctxCancel context.CancelFunc
 
 	for {
 		select {
 		case <-mEnabled.ClickedCh:
 			if mEnabled.Checked() {
 				mainCtxCancel()
+
 				mEnabled.Uncheck()
 				mPause.Disable()
 			} else {
 				runTwentyTwentyTwenty(notifier, duration, frequency, notificationSound)
+
 				mEnabled.Check()
 				mPause.Enable()
 			}
 		case <-mPause.ClickedCh:
 			if mPause.Checked() {
-				mainCtxCancel() // make sure the loop stopped
-				ctxCancel()
+				mainCtxCancel() // make sure the current twenty-twenty-twenty goroutine stopped
+				ctxCancel()     // cancel the current pause if it is running
 				runTwentyTwentyTwenty(notifier, duration, frequency, notificationSound)
+
 				mEnabled.Enable()
 				mPause.Uncheck()
 			} else {
-				log.Println("Pausing twenty-twenty-twenty for 1 hour...")
-				go func(ctx context.Context) {
-					mainCtxCancel()
-					timer := time.NewTimer(time.Hour)
-					select {
-					case <-timer.C:
-						notification := sendNotification(
-							notifier,
-							"Resuming 20-20-20",
-							fmt.Sprintf("You will see a notification every %.f minutes(s)", frequency.Minutes()),
-							notificationSound,
-						)
-						cancelNotificationAfter(notification, duration, notificationSound)
-						runTwentyTwentyTwenty(notifier, duration, frequency, notificationSound)
-						mEnabled.Enable()
-						mPause.Uncheck()
-					case <-ctx.Done():
-					}
-					ctxCancel()
-				}(ctx)
+				ctx, ctxCancel = context.WithCancel(context.Background())
+				go resumeTwentyTwentyTwentyAfter(ctx, ctxCancel, time.Hour, mEnabled, mPause)
+
 				mEnabled.Disable()
 				mPause.Check()
 			}
 		case <-mSound.ClickedCh:
 			if mSound.Checked() {
 				*notificationSound = false
+
 				mSound.Uncheck()
 			} else {
 				err := initNotification()
@@ -82,6 +99,7 @@ func onReady() {
 					log.Fatalf("Error while initialising sound: %v\n", err)
 				}
 				*notificationSound = true
+
 				mSound.Check()
 			}
 		case <-mQuit.ClickedCh:
