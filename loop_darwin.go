dc245f4423acd48d89540457fe52068d7d994ae0
Thiago Kenji Okada <thiagokokada@gmail.com>
2024-01-18 21:35:16 +0000

    Only generate the current architecture in macOS builds

--- loop_darwin.go
+++ loop_darwin.go
@@ -2,9 +2,9 @@
 // +build darwin
 
 //go:generate go vet ./...
-//go:generate sh -c "go run gioui.org/cmd/gogio -ldflags=\"-X 'main.version=$(git describe --tags --dirty)' -s -w\" -o bin/twenty-twenty-twenty -target macos -icon ./assets/eye.png -o TwentyTwentyTwenty.app ."
+//go:generate sh -c "go run gioui.org/cmd/gogio -arch=$(uname -m | sed 's|x86_64|amd64|') -target=macos -ldflags=\"-X 'main.version=$(git describe --tags --dirty)' -s -w\" -icon=./assets/eye.png -o=bin/TwentyTwentyTwenty.app ."
 // Signing the code with the adhoc certificate
-//go:generate sh -c "codesign -s - TwentyTwentyTwenty.app/*"
+//go:generate sh -c "codesign -s - bin/TwentyTwentyTwenty.app"
 
 package main
 
