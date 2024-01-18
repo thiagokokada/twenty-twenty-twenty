8344fc615a868a4b413ca7c19e6daba70f18678a
Thiago Kenji Okada <thiagokokada@gmail.com>
2024-01-18 22:17:56 +0000

    Remove go:build from loop_darwin

--- loop_darwin.go
+++ loop_darwin.go
@@ -1,6 +1,3 @@
-//go:build darwin
-// +build darwin
-
 //go:generate go vet ./...
 //go:generate sh -c "go run gioui.org/cmd/gogio -arch=$(uname -m | sed 's|x86_64|amd64|') -target=macos -ldflags=\"-X 'main.version=$(git describe --tags --dirty)' -s -w\" -icon=./assets/eye.png -o=bin/TwentyTwentyTwenty.app ."
 //go:generate cp assets/macos/TwentyTwentyTwenty.app/Contents/Info.plist bin/TwentyTwentyTwenty.app/Contents/Info.plist
